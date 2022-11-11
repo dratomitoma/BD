567baa701cf6f9c3832d2db549b7be6a361f18fe
Tonevanda <up202108863@up.pt>
2022-11-11 15:34:32 +0000

    not null tiny int e referencias bem feitas ig

--- criar.sql
+++ criar.sql
@@ -8,56 +8,55 @@ DROP TABLE IF EXISTS GRUPO;
 DROP TABLE IF EXISTS EQUIPA;
 DROP TABLE IF EXISTS JOGADOR;
 
-CREATE TABLE Jogador(
+CREATE TABLE JOGADOR(
     nomeJogador VARCHAR(255) NOT NULL PRIMARY KEY,
-    nGolos INT NOT NULL,
-    nacionalidade VARCHAR(255),
-    numero INT,
-    jogosJogados INT,
-    tempoJogado INT
+    nGolos TINYINT NOT NULL,
+    nacionalidade VARCHAR(255) NOT NULL,
+    numero TINYINT NOT NULL,
+    jogosJogados TINYINT NOT NULL,
+    tempoJogado TINYINT NOT NULL
 );
 
-CREATE TABLE Equipa(
-    nomeEquipa VARCHAR(255) PRIMARY KEY,
-    jogosJogados INT,
-    golosTotais INT,
-    nPontos INT,
-    classificacao INT,
-    golosMarcados INT,
-    golosSofridos INT,
-    vitorias INT,
-    derrotas INT,
-    empates INT 
+CREATE TABLE EQUIPA(
+    nomeEquipa VARCHAR(255) PRIMARY KEY NOT NULL,
+    jogosJogados TINYINT NOT NULL,
+    golosTotais TINYINT NOT NULL,
+    nPontos TINYINT NOT NULL,
+    classificacao TINYINT NOT NULL,
+    golosMarcados TINYINT NOT NULL,
+    golosSofridos TINYINT NOT NULL,
+    vitorias TINYINT NOT NULL,
+    derrotas TINYINT NOT NULL,
+    empates TINYINT NOT NULL
 );
 
-CREATE TABLE Grupo(
-    idGrupo INT PRIMARY KEY
+CREATE TABLE GRUPO(
+    idGrupo TINYINT NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE Estado(
-    tipoEstado VARCHAR(255) PRIMARY KEY
+CREATE TABLE ESTADO(
+    tipoEstado VARCHAR(255) NOT NULL PRIMARY KEY,
     CONSTRAINT check_tipoEstado CHECK (tipoEstado == 'grupos' OR tipoEstado == 'oitavos' OR tipoEstado == 'quartos' OR tipoEstado == 'semis' OR tipoEstado == 'final')
 );
 
-CREATE TABLE Jogo(
-    idJogo VARCHAR(255) PRIMARY KEY,
-    resultado VARCHAR(255),
-    nFaltas INT,
-    posseDeBola INT,
-    cartoesAmarelos INT,
-    cartoesVermelhos INT,
-    cantos INT,
-    remates INT,
-    rematesABaliza INT,
-    passesCompletos INT,
-    marcadores VARCHAR(255) REFERENCES JOGADOR,
-    eliminatoria VARCHAR(255) REFERENCES ESTADO
+CREATE TABLE JOGO(
+    idJogo VARCHAR(255) NOT NULL PRIMARY KEY,
+    resultado VARCHAR(255) NOT NULL,
+    nFaltas TINYINT NOT NULL,
+    posseDeBola TINYINT NOT NULL,
+    cartoesAmarelos TINYINT NOT NULL,
+    cartoesVermelhos TINYINT NOT NULL,
+    cantos TINYINT NOT NULL,
+    remates TINYINT NOT NULL,
+    rematesABaliza TINYINT NOT NULL,
+    passesCompletos TINYINT NOT NULL,
+    marcadores VARCHAR(255) NOT NULL REFERENCES JOGADOR(nomeJogador),
+    eliminatoria VARCHAR(255) NOT NULL REFERENCES ESTADO(tipoEstado)
 );
 
-.read povoar.sql
 
-SELECT * FROM Jogador;
-SELECT * FROM Equipa;
-SELECT * FROM Grupo;
-SELECT * FROM Estado;
-SELECT * FROM Jogo;
+SELECT * FROM JOGADOR;
+SELECT * FROM EQUIPA;
+SELECT * FROM GRUPO;
+SELECT * FROM ESTADO;
+SELECT * FROM JOGO;
